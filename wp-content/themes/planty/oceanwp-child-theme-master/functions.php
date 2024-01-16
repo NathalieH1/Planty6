95459e23bb4caa44ac49c64df0394c65e8f092be
Hnat <nathalie-humbert@outlook.com>
2024-01-16 15:37:29 +0100

    Rafraîchissement du fichier

--- wp-content/themes/planty/oceanwp-child-theme-master/functions.php
+++ wp-content/themes/planty/oceanwp-child-theme-master/functions.php
@@ -36,24 +36,6 @@ function oceanwp_child_enqueue_parent_style() {
 
 add_action( 'wp_enqueue_scripts', 'oceanwp_child_enqueue_parent_style' );
 
-      /*  function display_button_for_admin($items, $args){
-{
-    if ($args->theme_location === 'main_menu') {
-        preg_match_all('/<li[^>]*>.*?<\/li>/', $items, $matches);
-        $milieu = ceil(count($matches[0]) / 2);
-        $nouveau_bouton = '<li class="menu-item"><a href="http://localhost/Planty/wp-admin/index.php" class="admin-button">Admin</a></li>';
-        array_splice($matches[0], $milieu, 0, $nouveau_bouton);
-
-        // Reconstruire la chaîne avec le tableau modifié
-        $items = implode('', $matches[0]);
-
-        return $items;
-    }
-    if ($args->theme_location === 'footer_menu') {
-        return $items;
-    }
-}
-add_action('wp_nav_menu_items', 'display_button_for_admin', 10, 2);*/
 
 
 function display_button_for_admin($items, $args)
@@ -76,26 +58,3 @@ function display_button_for_admin($items, $args)
 
 add_filter('wp_nav_menu_items', 'display_button_for_admin', 10, 2);
 
-/*add_filter( 'wp_nav_menu_items', 'add_extra_item_to_nav_menu', 10, 2 );
-function add_extra_item_to_nav_menu( $items, $args ) {
-    if (is_user_logged_in() && $args->menu == 303) {
-        $items .= '<li><a href="'. get_permalink( get_option('woocommerce_myaccount_page_id') ) .'">My Account</a></li>';
-    }
-    elseif (!is_user_logged_in() && $args->menu == 303) {
-        $items .= '<li><a href="' . get_permalink( wc_get_page_id( 'myaccount' ) ) . '">Sign in  /  Register</a></li>';
-    }
-    return $items;
-}
-
-// Votre nouvelle fonction
-add_filter('wp_nav_menu_items', 'display_button_for_admin', 10, 2);
-function display_button_for_admin($items, $args)
-{
-    if ($args->theme_location === 'main_menu' && is_user_logged_in()) {
-        $menu_items = wp_get_nav_menu_items($args->menu);
-        $milieu = ceil(count($menu_items) / 2);
-        $nouveau_bouton = '<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-144"><a href="http://localhost/Planty/wp-admin/index.php" class="admin-button">Admin</a></li>';
-        $items = substr_replace($items, $nouveau_bouton, strpos($items, '</li>', (strpos($items, '</li>') * $milieu) / 2) + 5, 0);
-    }
-    return $items;
-}*/
